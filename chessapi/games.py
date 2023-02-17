f4b54c72b2e4f826fd740e5ae58fae47a9fcd2ac
Karan Kadam <karank@outlook.com>
2023-02-17 15:59:48 +1030

    feat: 🎸 add basic logic to routes

---
+++ chessapi/games.py
@@ -0,0 +1,119 @@
+"""
+A chess class representing a chess board
+"""
+from dataclasses import dataclass
+from typing import Dict
+import chess
+
+
+@dataclass
+class Game:
+    """
+    A class to represent a chess game
+    """
+
+    def __init__(
+        self,
+        state: str = None,
+        player1: str = "Player 1",
+        player2: str = "Player 2",
+        id: str = None,  # leave this here for get_copy()
+    ):
+        # generate a game id
+        self.id = self._generate_id()
+
+        # maintain a chess board
+        if state is None:
+            self.board = chess.Board()
+        else:
+            self.board = chess.Board(state)
+
+        # player names
+        self.player1 = player1
+        self.player2 = player2
+
+    def _generate_id(self) -> str:
+        """
+        Generate a simple incrementing ID for a new game
+        """
+        # maintain an incrementing ID
+        if not hasattr(Game, "id_counter"):
+            Game.id_counter = 0
+        Game.id_counter += 1
+
+        return str(Game.id_counter)
+
+    def get_copy(self) -> "Game":
+        """
+        Get a copy of the game
+        """
+        return Game(
+            id=self.id,
+            state=self.board.fen(),
+            player1=self.player1,
+            player2=self.player2,
+        )
+
+
+class Games:
+    """
+    A class to maintain a list of chess games
+    """
+
+    def __init__(self):
+        self.games: Dict[str, Game] = {}
+
+    def add_game(
+        self,
+        state: str = None,
+        player1: str = "Player 1",
+        player2: str = "Player 2",
+    ) -> None:
+        """
+        Add a new game to the list of games
+        """
+        game = None
+        if state is None:
+            game = Game(player1=player1, player2=player2)
+        else:
+            game = Game(state=state, player1=player1, player2=player2)
+
+        self.games[game.id] = game
+
+    def get_game(self, game_id: str) -> Game:
+        """
+        Get a game by ID
+        """
+        return self.games[game_id]
+
+    def update_game(self, game_id: str, state: str) -> None:
+        """
+        Update a game by ID
+        """
+        self.games[game_id].board = chess.Board(state)
+
+    def delete_game(self, game_id: str) -> None:
+        """
+        Delete a game by ID
+        """
+        temp_copy = None
+        if game_id in self.games:
+            # make a copy of the game
+            temp_copy = self.games[game_id].get_copy()
+
+            # delete the game
+            del self.games[game_id]
+
+        return temp_copy
+
+    def get_games(self) -> Dict[str, Game]:
+        """
+        Get a list of all games
+        """
+        return self.games
+
+    def get_game_state(self, game_id: str) -> str:
+        """
+        Get the state of a game by ID
+        """
+        return self.games[game_id].board.fen()
--- chessapi/main.py
+++ chessapi/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Union
+from typing import Dict, List, Union
 
 from fastapi import FastAPI
 
@@ -12,50 +12,68 @@ from .models import (
     GamesPostResponse,
 )
 
+from .games import Game, Games
+
 app = FastAPI(
-    title='Chess Game API',
-    version='1.0.0',
-    servers=[{'url': 'http://localhost:8000'}],
+    title="Chess Game API",
+    version="1.0.0",
+    servers=[{"url": "http://localhost:8000"}],
 )
 
+ALL_GAMES = Games()
 
-@app.get('/games', response_model=List[GamesGetResponse])
+
+@app.get("/games", response_model=List[GamesGetResponse])
 def get_games() -> List[GamesGetResponse]:
     """
     Get a list of all games
     """
-    pass
+    return ALL_GAMES.get_games()
 
 
 @app.post(
-    '/games', response_model=None, responses={'201': {'model': GamesPostResponse}}
+    "/games", response_model=None, responses={"201": {"model": GamesPostResponse}}
 )
 def post_games(body: GamesPostRequest) -> Union[None, GamesPostResponse]:
     """
     Create a new game
     """
-    pass
+
+    # create a new game
+    new_game = Game(state=body.state, player1=body.player1, player2=body.player2)
+
+    # add the game to the list of games
+    ALL_GAMES.add_game(new_game)
+
+    # return the new game's ID
+    return GamesPostResponse(id=new_game.id)
 
 
-@app.get('/games/{game_id}', response_model=GamesGameIdGetResponse)
+@app.get("/games/{game_id}", response_model=GamesGameIdGetResponse)
 def get_games_game_id(game_id: str) -> GamesGameIdGetResponse:
     """
     Get the details of a game by ID
     """
-    pass
+    game = ALL_GAMES.get_game(game_id)
+    return GamesGameIdGetResponse(
+        id=game.id,
+        state=game.board.fen(),
+        player1=game.player1,
+        player2=game.player2,
+    )
 
 
-@app.put('/games/{game_id}', response_model=None)
+@app.put("/games/{game_id}", response_model=None)
 def put_games_game_id(game_id: str, body: GamesGameIdPutRequest = ...) -> None:
     """
     Update the state of a game by ID
     """
-    pass
+    ALL_GAMES.update_game(game_id, body.state)
 
 
-@app.delete('/games/{game_id}', response_model=None)
+@app.delete("/games/{game_id}", response_model=None)
 def delete_games_game_id(game_id: str) -> None:
     """
     Delete a game by ID
     """
-    pass
+    ALL_GAMES.delete_game(game_id)
